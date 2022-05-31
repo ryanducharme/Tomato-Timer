51624f0735c5cd3ac2a9e821e46f3176a5d1dc9e
Ryan DuCharme <ryan.ducharme77@gmail.com>
2022-05-30 22:22:35 -0400

    Update server.js

--- Tomato Timer/server.js
+++ Tomato Timer/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const { json } = require('express/lib/response');
+const { ObjectId } = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const port = 3000;
 
@@ -40,12 +41,17 @@ MongoClient.connect('mongodb+srv://rjdev-test:J9ukuNGfWZWlEsRt@cluster0.b7crfg0.
     });
 
     app.get('/tasks/:id', (req, res) => {
-      userDataCollection.findOne()
+      console.log(req.params.id);
+      userDataCollection.findOne({ _id: ObjectId(req.params.id) })
         .then(result => {
           res.json(result);
+
         })
         .catch(error => console.error(error))
     });
+
+
+
 
     app.post('/tasks', (req, res) => {
       userDataCollection.insertOne(
@@ -57,8 +63,15 @@ MongoClient.connect('mongodb+srv://rjdev-test:J9ukuNGfWZWlEsRt@cluster0.b7crfg0.
         .catch(error => console.error(error))
     });
 
-    app.delete('/tasks:id', (req, res) => {
-
+    app.delete('/tasks/:id', (req, res) => {
+      userDataCollection.deleteOne({ _id: ObjectId(req.params.id) })
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('Nothing to delete')
+          }
+          res.json(`Deleted ${req.params.id}`)
+        })
+        .catch(error => console.error(error))
     });
 
     app.listen(port, () => {
